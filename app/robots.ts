579868509c22d77032cdf526bc12dbffa72855fb
Abdallah Megahed <abd027282930@gmail.com>
2026-09-09 05:17:29 +0300

    Update robots to block removed routes

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,15 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/Cart"],
+      disallow: [
+        "/Cart",
+        "/Auth",
+        "/Register",
+        "/profile",
+        "/checkout",
+        "/admin",
+        "/contact",
+      ],
     },
     sitemap: "https://supermarket-hazel.vercel.app/sitemap.xml",
   };
