c3061a621a16433084757cf7a8fe8bda19561797
Abdullah Megahed <abd027282930@gmail.com>
2026-09-13 20:57:39 +0300

    Adjust margin for HomePageClient section

--- components/HomePageClient.tsx
+++ components/HomePageClient.tsx
@@ -32,7 +32,7 @@ export default function HomePageClient() {
   return (
     <div className="overflow-x-hidden">
       <Nav />
-      <section className="mt-10" >
+      <section className="mt-0 lg:mt-10" >
         <HeroSection />
       </section>
 
